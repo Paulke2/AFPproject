45974f8df6d63d4626ccf7ec546ae82c95730903
Paul Kearney <paulke@udel.edu>
2023-07-31 18:43:24 -0400

    styled searchbar

--- Client/src/Components/Searchbar.js
+++ Client/src/Components/Searchbar.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Form,Button } from "react-bootstrap";
 import "./Searchbar.css";
-
+import searchIcon from "../pictures/search_icon.png";
 const SearchBar = (props) => {
     return (
         <div className="wrap">
             <div className="search">
                 <Form.Group controlId="editName">
                     <Form.Control
+                    style={{color:"black"}}
                         className="searchBar"
                         value={props.projectSearch}
                         placeholder="search"
@@ -15,7 +16,11 @@ const SearchBar = (props) => {
                             event
                         ) => props.setProjectSearch(event.target.value)}
                     />
+                    
                 </Form.Group>
+                <div className="searchButton">
+                <img src={searchIcon} className="searchIcon"/>
+                </div>
             </div>
            
         </div>
