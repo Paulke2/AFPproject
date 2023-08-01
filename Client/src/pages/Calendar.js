e1e0e3d23bc786cf14a46777e8ee9aaab5cd829c
Paul Kearney <paulke@udel.edu>
2023-08-01 15:19:11 -0400

    added employee list to calendar

--- Client/src/pages/Calendar.js
+++ Client/src/pages/Calendar.js
@@ -1,5 +1,64 @@
+import ListGroup from 'react-bootstrap/ListGroup';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import logo from "../pictures/AFPlogo.png"
+import { useNavigate, useParams } from "react-router-dom";
+import './Calendar.css'
+import Badge from 'react-bootstrap/Badge';
 const Calendar = () =>{
-return(<>here i plan to make a calendar to schedule ppl to projects. option to complete week which totals ppls hours at each
-job for the week. output cvs/exel file for each month if user presses a button. </>);
+    const navigate = useNavigate();
+return(<div> 
+<Navbar  style={{
+                backgroundColor: "#90ee90",
+                paddingLeft: "16px",
+                paddingRight: "16px",
+                boxShadow: "0 4px 6px -6px #222"
+            }}bg="dark" data-bs-theme="dark">
+          
+          <Nav className="me-auto"></Nav>
+          <Nav.Link onClick={() => navigate("/Calendar")} style={{color:"white",fontStyle:"oblique"}}>Calendar</Nav.Link>
+          <Navbar.Brand
+            style={{ cursor: "pointer",marginLeft:"20px"}}
+            onClick={() => navigate("/")}
+          >
+                        <img className="logo"src={logo}/>
+          </Navbar.Brand>
+      </Navbar>
+
+      <Row>
+        <Col className='col-2 employeeList' >
+        <ListGroup as="ul">
+      <ListGroup.Item action  >
+        Paul Kearney
+        <Badge bg="primary" pill>
+          14
+        </Badge>
+      </ListGroup.Item>
+      <ListGroup.Item action >other Kearney
+      <Badge bg="primary" pill>
+          14
+        </Badge></ListGroup.Item>
+      <ListGroup.Item action >
+      example Kearney
+      <Badge bg="primary" pill>
+          14
+        </Badge>
+      </ListGroup.Item>
+      <ListGroup.Item action >place holdington
+      <Badge bg="primary" pill>
+          14
+        </Badge></ListGroup.Item>
+    </ListGroup>
+        </Col>
+        <Col className='col-10'>
+        test
+        </Col>
+      </Row>
+
+    
+ </div>);
 }
 export default Calendar;
