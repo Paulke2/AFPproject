c239ec9df0116cb4fa96de491ca3fcd2dd4a04a6
Paulke2 <paulke2@udel.edu>
2023-08-05 09:22:01 -0400

    no time card shown if one is not made for the week

--- Client/src/Components/DisplayTimeCard.js
+++ Client/src/Components/DisplayTimeCard.js
@@ -2,9 +2,9 @@ import { relativeTimeRounding } from "moment";
 
 
 const DisplayTimeCard = (props) => {
-  return( <div>
+  return( props.currentTimeCard!==null?<div>
     Name:{props?.currentTimeCard?.employeeName} Hours:{props?.currentTimeCard?.totalHours}
-  </div>);
+  </div>:<div></div>);
 };
 
 export default DisplayTimeCard;
