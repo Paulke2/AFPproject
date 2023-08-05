b059a896f55da8e4efd01d33275b9cfbc64bf572
Paulke2 <paulke2@udel.edu>
2023-08-05 10:21:24 -0400

    display time function for getting hours for each job

--- Client/src/Components/DisplayTimeCard.js
+++ Client/src/Components/DisplayTimeCard.js
@@ -1,10 +1,49 @@
-import { relativeTimeRounding } from "moment";
-
+import "./DisplayTimeCard.css";
+import { useState, useEffect } from "react";
 
 const DisplayTimeCard = (props) => {
-  return( props.currentTimeCard!==null?<div>
-    Name:{props?.currentTimeCard?.employeeName} Hours:{props?.currentTimeCard?.totalHours}
-  </div>:<div></div>);
+  //this state list loops through time card job
+  //entries,checks for capitalization mistakes in job names, and combines the
+  //hours for 2 entries if the capitalization differs
+  const [jobsForWeek, setJobsForWeek] = useState({});
+  useEffect(() => {
+    const newJobsForWeek = {};
+
+    const daysOfWeek = [
+      'Sunday', 'Monday', 'Tuesday', 'Wednesday',
+      'Thursday', 'Friday', 'Saturday'
+    ];
+
+    daysOfWeek.forEach(day => {
+      if (props.currentTimeCard?.[day]) {
+        props.currentTimeCard[day].split(",").forEach((jobEntry) => {
+          const [jobName, jobHours] = jobEntry.split("-");
+          const parsedJobHours = parseInt(jobHours);
+
+          newJobsForWeek[jobName.toLowerCase()] =
+            (newJobsForWeek[jobName.toLowerCase()] || 0) + parsedJobHours;
+        });
+      }
+    });
+
+    setJobsForWeek(newJobsForWeek);
+  }, [props.currentTimeCard]);
+
+
+  return props.currentTimeCard !== null ? (
+    <div className="timeCard">
+      <h5>{props?.currentTimeCard?.employeeName} </h5>
+      <ul>
+        {Object.entries(jobsForWeek).map(([jobName, jobHours]) => (
+          <li key={jobName}>
+            {jobName}: {jobHours} hours
+          </li>
+        ))}
+      </ul>
+    </div>
+  ) : (
+    <div></div>
+  );
 };
 
 export default DisplayTimeCard;
