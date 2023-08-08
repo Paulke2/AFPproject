bd36592c47d13e058e7198562629527b70ace2a4
Paulke2 <paulke2@udel.edu>
2023-08-08 11:04:16 -0400

    added office location for job and badge now displays standard hours for office worker

--- Client/src/Components/GetEmployees.js
+++ Client/src/Components/GetEmployees.js
@@ -11,44 +11,47 @@ const GetEmployees = (props) => {
 
   useEffect(() => {
     startOfDesiredWeek = moment(props.dateToCheck).startOf("isoWeek");
-    console.log(startOfDesiredWeek.format('l'));
+    console.log(startOfDesiredWeek.format("l"));
   }, [props.dateToCheck]);
-
-  
 
   return (
     <>
-      
-     
-        <ListGroup as="ul">
-          {props.employees &&
-            props.employees.map((employee) => (
-              <ListGroup.Item
-                action
-                key={employee.id}
-                style={{
-                  backgroundColor:
-                    employee.employeeName === (props.currentEmployee && props.currentEmployee.employeeName)
-                      ? "#C62323"
-                      : "white",
-                  fontWeight: employee.employeeName === (props.currentEmployee && props.currentEmployee.employeeName)
+      <ListGroup as="ul">
+        {props.employees &&
+          props.employees.map((employee) => (
+            <ListGroup.Item
+              action
+              key={employee.id}
+              style={{
+                backgroundColor:
+                  employee.employeeName ===
+                  (props.currentEmployee && props.currentEmployee.employeeName)
+                    ? "#C62323"
+                    : "white",
+                fontWeight:
+                  employee.employeeName ===
+                  (props.currentEmployee && props.currentEmployee.employeeName)
                     ? "bold"
-                    : "200"
-                }}
-                onClick={() => {
-                  props.setCurrentEmployee(employee);
-                  props.setCurrentTimeCard(null);
-                }}
-              >
-                {employee.employeeName}
-                <Badge className="employeeBadge" bg="dark" pill>
-                  {props?.currentWeekCards[employee.employeeName]?.totalHours||0}
-                </Badge>
-              </ListGroup.Item>
-            ))}
-        </ListGroup>
+                    : "200",
+              }}
+              onClick={() => {
+                props.setCurrentEmployee(employee);
+                props.setCurrentTimeCard(null);
+              }}
+            >
+              {employee.employeeName}
+              <Badge className="employeeBadge" bg="dark" pill>
+                {props?.currentWeekCards[employee.employeeName]?.totalHours
+                  ? props?.currentWeekCards[employee.employeeName]?.totalHours
+                  : employee.officeWorker
+                  ? employee.officeWorkerHours * 5
+                  : 0}
+              </Badge>
+            </ListGroup.Item>
+          ))}
+      </ListGroup>
     </>
   );
-}
+};
 
 export default GetEmployees;
--- Client/src/pages/Home.js
+++ Client/src/pages/Home.js
@@ -16,12 +16,12 @@ const determineShow = (projectSearch, name, projectID) => {
   //this function takes projectsearch as a prop
   //and if it matches the job location/project name/project id
   // it will show the project
-  return projectID
+  return (projectID
     .toString()
     .toLowerCase()
     .includes(projectSearch.toString().toLowerCase()) ||
     name.toLowerCase().includes(projectSearch) ||
-    projectSearch === ""
+    projectSearch === "")&& name !== "Office"
     ? true
     : false;
 };
