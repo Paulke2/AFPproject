1be55c1d28ffc175a2ceff8b2222e201a8ddbf16
Paulke2 <paulke2@udel.edu>
2023-08-09 13:32:04 -0400

    pushing changes

--- Client/src/Components/TimeCardOptions.js
+++ Client/src/Components/TimeCardOptions.js
@@ -3,6 +3,13 @@ import { Button } from "react-bootstrap";
 import "./TimeCardOptions.css";
 import Export from "./Export";
 const TimeCardOptions = (props) => {
+
+  const handleDelete=()=>{
+//we need to pass current employee and current time card, remove curr time card from timecard
+//array, then patch employee with new time cards
+
+
+  }
   //the options right now will be delete, with a form to
   //confirm and an export week/month card
   return (
@@ -11,7 +18,7 @@ const TimeCardOptions = (props) => {
       <Export currentTimeCard={props.currentTimeCard}/>
      <br></br>
      <br></br>
-      <Button variant="danger">delete</Button>
+      <Button variant="danger"onClick={handleDelete}>delete</Button>
       </div>
     </div>
   );
