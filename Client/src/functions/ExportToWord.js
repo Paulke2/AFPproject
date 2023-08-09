7e56039c52298e7c3e7fade71d9d04c73fc890dc
Paulke2 <paulke2@udel.edu>
2023-08-09 13:27:40 -0400

    added table to word

--- Client/src/functions/ExportToWord.js
+++ Client/src/functions/ExportToWord.js
@@ -1,24 +1,38 @@
-import { Paragraph, Document, Packer } from "docx";
+import { Paragraph, Document, Packer,TableRow ,TableCell,Table} from "docx";
 import { saveAs } from "file-saver";
 const ExportToWord = (currentTimeCard) => {
     console.log("Current Time Card:", currentTimeCard);
+    const tableToAddToDocument = new Table({
+        rows: [
+            new TableRow({
+                children: [
+                    new TableCell({
+                        children: [new Paragraph("Row 1 Cell 1 ")],
+                    }),
+                    new TableCell({
+                        children: [new Paragraph("Row 1 Cell 2 ")],
+                    }),
+                ],
+            }),
+            new TableRow({
+                children: [
+                    new TableCell({
+                        children: [new Paragraph("Row 2 Cell 1 ")],
+                    }),
+                    new TableCell({
+                        children: [new Paragraph("Row 2 Cell 2 ")],
+                    }),
+                ],
+            }),
+        ],
+    });
     const doc = new Document({
       sections: [
         {
-          children: [
-            new Paragraph({
-              text: "test",
-              bullet: {
-                level: 0 //How deep you want the bullet to be
-              }
-            }),
-            new Paragraph({
-              text: "testing",
-              bullet: {
-                level: 0
-              }
-            })
-          ]
+            children: [
+                new Paragraph({ text: "Table with third row added to it" }),
+                tableToAddToDocument,
+            ],
         }
       ]
     });
