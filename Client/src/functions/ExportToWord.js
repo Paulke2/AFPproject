7c2b1d360a516be4e7cf5c1a5a9da0296cfc05eb
Paul Kearney <paulke@udel.edu>
2023-08-09 17:38:53 -0400

    pushing changes

--- Client/src/functions/ExportToWord.js
+++ Client/src/functions/ExportToWord.js
@@ -1,53 +1,39 @@
-import { Paragraph, Document, Packer, TableRow, TableCell, Table } from "docx";
+import { Paragraph, Document, Packer, TableRow, TableCell, Table,WidthType ,TextRun} from "docx";
 import { saveAs } from "file-saver";
 
 const ExportToWord = (currentTimeCard) => {
     console.log("Current Time Card:", currentTimeCard);
     
 
-    const doc = new Document({
-        sections: [
-            {
-                children: [
-                    new Table({
-                        rows: [
-                            new TableRow({
-                                children: [
-                                    new TableCell({
-                                        width: { size: 100 / 2, type: "dxa" }, // Set width in twips (dxa)
-                                        children: [new Paragraph("Row 1 Cell 1")],
-                                    }),
-                                    new TableCell({
-                                        width: { size: 100, type: "dxa" }, // Set width in twips (dxa)
-                                        children: [new Paragraph("Row 1 Cell 2")],
-                                    }),
-                                ],
-                            }),
-                           
-                            // ... (similarly for other rows)
-                        ],
-                    })
-                ],
-            },
-        ],
-    });
-    doc.addSection({
-        children: [
+    const table = new Table({
+        rows: [
             new TableRow({
-                width: {
-                    size: 50, // Change this value as needed
-                    type: "dxa",
-                },
+                children: [
+                    new TableCell({
+                        children: [new Paragraph("Hello")],
+                    }),
+                    new TableCell({
+                        children: [],
+                    }),
+                ],
+            }),
+            new TableRow({
                 children: [
                     new TableCell({
                         children: [],
                     }),
                     new TableCell({
-                        children: [new Paragraph("New Section also added")],
+                        children: [new Paragraph("World")],
                     }),
                 ],
             }),
         ],
+    });
+    
+    const doc = new Document({
+        sections: [{
+            children: [table],
+        }],
     });
 
     Packer.toBlob(doc).then((blob) => {
