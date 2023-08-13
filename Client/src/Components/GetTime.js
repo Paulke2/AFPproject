c991f4a20708fe30dc5671534103b629ee52e4ff
Paul Kearney <paulke@udel.edu>
2023-08-13 19:01:08 -0400

    weeks run from sun to sat now

--- Client/src/Components/GetTime.js
+++ Client/src/Components/GetTime.js
@@ -3,19 +3,24 @@ import { Button } from "react-bootstrap";
 import leftArrow from "../pictures/pointsLeft.png"
 import rightArrow from "../pictures/pointsRight.png"
 const moment = require("moment");
+moment.updateLocale("en", {
+  week: {
+    dow: 0, // Sunday
+  },
+});
+
 const GetTime = (props) =>{
     
-    //const DateToCheck =moment().format('L');
   
     // Create a moment object with the specifieD date
     const specificDate = moment(props.dateToCheck);
   
     // Get the start of the week (Sunday in the US, Monday in other regions)
     const startOfWeek = specificDate.clone().startOf("isoWeek");
-  
+    const sundayOffSet = startOfWeek.clone().subtract(1,"days");
     // Get the end of the week (Saturday in the US, Sunday in other regions)
     const endOfWeek = specificDate.clone().endOf("isoWeek");
-  
+    const SaturdayOffSet = endOfWeek.clone().subtract(1,"days");
     // Now you have the start and end dates of the specified week
   
     return(<><img src={leftArrow}style={{paddingRight:"20%",cursor: "pointer",height:"50px",paddingTop:"5px"}}
@@ -24,8 +29,8 @@ const GetTime = (props) =>{
           props.setCurrentTimeCard(null);
         }}
       ></img>
-      {startOfWeek.format("l").toString()} -{" "}
-      {endOfWeek.format("l").toString()}
+      {sundayOffSet.format("l").toString()} -{" "}
+      {SaturdayOffSet.format("l").toString()}
       <img src={rightArrow} style={{paddingLeft:"20%",cursor: "pointer",height:"50px",paddingTop:"5px"}}
         onClick={() => {
           props.setDateToCheck(endOfWeek.add(1, "days"));
