958d82a770a88e51b4d421a1575ebee0b76117f2
Paul Kearney <paulke@udel.edu>
2023-08-13 18:54:13 -0400

    working on nested col in word.

--- Client/src/functions/ExportToWord.js
+++ Client/src/functions/ExportToWord.js
@@ -1,46 +1,70 @@
-const docx = require('docx');
+const docx = require("docx");
 const { Document, Packer, Paragraph, Table, TableCell, TableRow } = docx;
 import { saveAs } from "file-saver";
 
 const ExportToWord = (currentTimeCard) => {
     console.log("Current Time Card:", currentTimeCard);
-
+  
+    const NestedTable = new Table({
+      rows: [
+        new TableRow({
+          children: [
+            new TableCell({
+              children: [new Paragraph("Nested")],
+            }),
+            new TableCell({
+              children: [new Paragraph("row")],
+            }),
+          ],
+        }),
+        new TableRow({
+          children: [
+            new TableCell({
+              children: [new Paragraph("Nested")],
+            }),
+            new TableCell({
+              children: [new Paragraph("Row")],
+            }),
+          ],
+        }),
+      ],
+    });
+  
     const table = new Table({
-        rows: [
-            new TableRow({
-                children: [
-                    new TableCell({
-                        children: [new Paragraph("Hello")],
-                    }),
-                    new TableCell({
-                        children: [],
-                    }),
-                ],
+      rows: [
+        new TableRow({
+          children: [
+            new TableCell({
+              children: [new Paragraph("Hello22")],
             }),
-            new TableRow({
-                children: [
-                    new TableCell({
-                        children: [],
-                    }),
-                    new TableCell({
-                        children: [new Paragraph("World")],
-                    }),
-                ],
+            new TableCell({
+              children: [], // You can add content here if needed
             }),
-        ],
+          ],
+        }),
+        new TableRow({
+          children: [
+            new TableCell({
+              children: [NestedTable], // Add the NestedTable as a cell content
+            }),
+          ],
+        }),
+      ],
     });
-    
+  
     const doc = new Document({
-        sections: [{
-            children: [table],
-        }],
+      sections: [
+        {
+          children: [table],
+        },
+      ],
     });
-
+  
     Packer.toBlob(doc).then((blob) => {
-        console.log(blob);
-        saveAs(blob, "example.docx");
-        console.log("Document created successfully");
+      console.log(blob);
+      saveAs(blob, "example.docx");
+      console.log("Document created successfully");
     });
-};
-
-export default ExportToWord;
+  };
+  
+  export default ExportToWord;
