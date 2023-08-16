d6303cdbf9adee3ccb6c13501801a47ba1233b72
Paulke2 <paulke2@udel.edu>
2023-08-16 11:26:09 -0400

    can now edit projects in modal

--- Client/src/Components/WeekCard.js
+++ Client/src/Components/WeekCard.js
@@ -1,95 +1,192 @@
-
 import React from "react";
-import { Card } from "react-bootstrap";
-import { Form ,Button} from "react-bootstrap";
-import { useEffect,useRef } from "react";
+import { Card, Modal } from "react-bootstrap";
+import { Form, Button } from "react-bootstrap";
+import { useState, useEffect, useRef } from "react";
 import "./WeekLayout.css";
-import { Typeahead } from 'react-bootstrap-typeahead';
+import { Typeahead } from "react-bootstrap-typeahead";
 const WeekCard = ({
-    dayTitle,
-    jobList,
-    setJobList,
-    jobString,
-    jobNumber,
-    onJobStringChange,
-    onJobNumberChange,
-    onSave,
-    totalHours,
-    ProjectNames
-  })=>{
-   
-    const eventRef = useRef(null); // Create a ref to hold the event
+  dayTitle,
+  jobList,
+  setJobList,
+  jobString,
+  jobNumber,
+  onJobStringChange,
+  onJobNumberChange,
+  onSave,
+  totalHours,
+  ProjectNames,
+}) => {
+  const [overTime, setOverTime] = useState("No");
+  const [selectedJob, setSelectedJob] = useState("");
+  const [selectedJobHours, setSelectedJobHours] = useState(0);
+  const eventRef = useRef(null); // Create a ref to hold the event
+  const [show, setShow] = useState(false);
 
-    const deleteJobFromList = async (jobToDelete, oldJobList, event) => {
-      const newJobList = oldJobList.filter(job => job !== jobToDelete);
-      await setJobList(newJobList);
-      eventRef.current = event; // Store the event in the ref
-    };
+  const handleClose = () => setShow(false);
+  // const handleShow = () => setShow(true);
+  const handleShow = (job) => {
+    setSelectedJob(job); // Store the clicked job in the state
+    setShow(true); // Show the modal
+  };
+
+  const deleteJobFromList = async (jobToDelete, oldJobList, event) => {
+    const newJobList = oldJobList.filter((job) => job !== jobToDelete);
+    await setJobList(newJobList);
+    eventRef.current = event; // Store the event in the ref
+  };
+  const saveNewJobList = async(jobToAdd,oldJobList,event) => {
+    if(!oldJobList.includes(jobToAdd)){
+      console.log("trying to delete:"+selectedJob)
+      console.log("trying to add:"+jobToAdd)
+      console.log(oldJobList)
+      let newJobList = oldJobList.filter((job) => job !== selectedJob);
+      console.log(newJobList)
+     newJobList = [...newJobList,jobToAdd]
+    await setJobList(newJobList);
+    console.log("saving job:");
+    console.log(jobToAdd);
+    console.log(newJobList);
+    eventRef.current = event; // Store the event in the ref
+    }
+  };
+  useEffect(() => {
+    if (eventRef.current) {
+      onSave(eventRef.current); // Call onSave with the stored event
+      eventRef.current = null;
+    }
+  }, [jobList, onSave]);
+  let index=selectedJob.lastIndexOf("-");
+  useEffect(()=>{setSelectedJobHours(selectedJob.slice(index+1))
+  console.log("hours:")
+  console.log(selectedJobHours)},[index])
+  return (
+    <>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>{selectedJob.slice(0,index)}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
   
-    useEffect(() => {
-      if (eventRef.current) {
-        onSave(eventRef.current); // Call onSave with the stored event
-        eventRef.current = null;
-      }
-    }, [jobList, onSave]);
-    
-    return (
-      <Card  style={{ height: "100%",padding:"0px" }}>
-      <Card.Body style={{ padding:"0px" }}>
-        <div  style={{ height: "100%"  }}>
-          <Card.Title>{dayTitle}</Card.Title>
-          <div style={{paddingLeft:"2px"}}>
-          <ul style={{ listStylePosition: "inside", margin: 0, padding: 0 }}>
-        {jobList?.map((job) => (
-          <li key={job} style={{ display: "flex", alignItems: "center", justifyContent: "space-between", padding: "5px 0" }}>
-          <>
-            <span style={{marginLeft:"0px",paddingLeft:"0px"}}>{job}</span>
-            <Button
-              variant="danger"
-              className="DeleteJobButton"
-            
-              onClick={(event) => {
-                deleteJobFromList(job, jobList, event);
-              }}
-            >
-              -
-            </Button>
-            </>
-          </li>
-        ))}
-      </ul>
-          </div>
-          <div className="formsForJob" style={{ flexGrow: 1 }}>
-          <Form.Group className="d-flex">
-  <Typeahead
-    labelKey="name"
-    id="job locatoin select"
-    onChange={onJobStringChange}
-    style={{ color: "black", width: "60%" }}
-    options={ProjectNames}
-    placeholder="Jobs"
-    selected={jobString ? [jobString] : []} // Convert jobString to an array or use an empty array
+          
+        <div style={{display:"flex",alignItems:"center"}}>
+          Adjust Hours:  
+              <Form.Control
+                type="number"
+                value={selectedJobHours}
+                style={{width:"80px",marginLeft:"5px"}}
+                onChange={(event) => setSelectedJobHours(event.target.value)}
+                autocomplete="off"
+                
+              />
+            </div>
+            <br></br>
+        <Form style={{ display: "flex" }}>
+          <br></br>
+          <br></br>
+  <span style={{ marginRight: "10px" }}>Was This Over Time?</span>
+  <Form.Check
+    type="radio"
+    id="custom-switch-yes"
+    name="overTimeOption"
+    value="Yes"
+    onClick={() => setOverTime("Yes")}
+    label="Yes"
+    style={{ marginRight: "10px" }}
   />
-  <Form.Control
-    type="number"
-    style={{ color: "black", width: "40%" }}
-    value={jobNumber}
-    placeholder="Hours"
-    onChange={onJobNumberChange}
-    className="no-spinners"
+  <Form.Check
+    type="radio"
+    name="overTimeOption"
+    value="No"
+    onClick={() => setOverTime("No")}
+    checked={overTime === "No"}
+    label="No"
+    id="custom-switch-no"
+    style={{ marginRight: "10px" }}
   />
-</Form.Group>
-          </div>
-          <Button variant="success" onClick={onSave}>
-            Save
+</Form>
+          <Button
+            variant="danger"
+            onClick={(event) => {
+              deleteJobFromList(selectedJob, jobList, event);
+            }}
+          >
+            Delete Job Entry
           </Button>
-        </div>
-      </Card.Body>
-      <Card.Footer>
-        <small className="text-muted">Hours: {totalHours}</small>
-      </Card.Footer>
-    </Card>
-      );
-    
-}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="success" onClick={(event)=>saveNewJobList(selectedJob.slice(0,index)+"-"+selectedJobHours.toString(),jobList,event)}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <Card style={{ height: "100%", padding: "0px" }}>
+        <Card.Body style={{ padding: "0px" }}>
+          <div style={{ height: "100%" }}>
+            <Card.Title>{dayTitle}</Card.Title>
+            <div style={{ paddingLeft: "2px" }}>
+              <ul
+                style={{ listStylePosition: "inside", margin: 0, padding: 0 }}
+              >
+                {jobList?.map((job) => (
+                  <li
+                    key={job}
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "space-between",
+                      padding: "5px 0",
+                    }}
+                  >
+                    <>
+                      <span
+                        style={{
+                          marginLeft: "0px",
+                          paddingLeft: "0px",
+                          cursor: "pointer",
+                        }}
+                        onClick={() => handleShow(job)}
+                      >
+                        {job}
+                      </span>
+                    </>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="formsForJob" style={{ flexGrow: 1 }}>
+              <Form.Group className="d-flex">
+                <Typeahead
+                  labelKey="name"
+                  id="job locatoin select"
+                  onChange={onJobStringChange}
+                  style={{ color: "black", width: "60%" }}
+                  options={ProjectNames}
+                  placeholder="Jobs"
+                  selected={jobString ? [jobString] : []} // Convert jobString to an array or use an empty array
+                />
+                <Form.Control
+                  type="number"
+                  style={{ color: "black", width: "40%" }}
+                  value={jobNumber}
+                  placeholder="Hours"
+                  onChange={onJobNumberChange}
+                  className="no-spinners"
+                />
+              </Form.Group>
+            </div>
+            <Button variant="success" onClick={onSave}>
+              Save
+            </Button>
+          </div>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Hours: {totalHours}</small>
+        </Card.Footer>
+      </Card>
+    </>
+  );
+};
 export default WeekCard;
--- Client/src/pages/DesignJobs.js
+++ Client/src/pages/DesignJobs.js
@@ -1,7 +1,9 @@
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import logo from "../pictures/AFPlogo.png";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom"
+import { Col, Row } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 import GetJobList from "../Components/DesignJobPageComponents/GetJobList"
 const DesignJobs = (props)=>{
 //make it so ppl can claim jobs. have a backlog, ordered by date or priority
@@ -38,7 +40,45 @@ return (<> <Navbar
       <img className="logo" src={logo} />
     </Navbar.Brand>
   </Navbar>
+  <Row style={{height:"100%",width:"100%"}}>
+  <Col className="col-2 employeeList">
   <div><GetJobList designJobNames={props.designJobNames}/></div>
+  </Col>
+  <Col className="col-10">
+  <Card style={{ width: '18rem'}}>
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      
+      </Card.Body>
+    </Card>
+    <Card style={{ width: '18rem'}}>
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      
+      </Card.Body>
+    </Card><Card style={{ width: '18rem'}}>
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      
+      </Card.Body>
+    </Card>
+  </Col>
+  </Row>
   </>);
 }
 
