928cd73cbcbc8b87cd4e4b94a1b60fb974aea360
Paulke2 <paulke2@udel.edu>
2023-08-17 14:29:07 -0400

    design jobs can now be dragged/dropped. working on between containers

--- Client/src/Components/DesignJobPageComponents/GetJobList.js
+++ Client/src/Components/DesignJobPageComponents/GetJobList.js
@@ -1,34 +1,57 @@
 import ListGroup from "react-bootstrap/ListGroup";
 import { Badge } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 import { useEffect, useState } from "react";
 import "../GetEmployee.css";
-
+import { DragDropContext,Droppable , Draggable} from 'react-beautiful-dnd';
 const moment = require("moment");
 
 const GetJobList = (props) => {
     const [selectedJob,setSelectedJob]=useState(null);
   let startOfDesiredWeek = moment(props.dateToCheck).startOf("isoWeek");
-
+  const [jobList, setJobList] = useState(props.designJobNames);
 console.log(props.designJobNames);
+const handleOnDragEnd = (result)=>{
+  if (!result.destination) return;
+  console.log(result)
+    const newTasks = Array.from(jobList);
+    const [reorderedTask] = newTasks.splice(result.source.index, 1);
+    newTasks.splice(result.destination.index, 0, reorderedTask);
+
+    setJobList(newTasks);
+}
+
   return (
-    <>
-      <ListGroup as="ul">
-        {props.designJobNames &&
-          props.designJobNames.map((job) => (
-            <ListGroup.Item
-             
-              key={job}
-              
-              
-            >
-              {job}
-              <Badge className="employeeBadge" bg="dark" pill>
-                4
+    <DragDropContext onDragEnd={handleOnDragEnd}>
+    <Droppable droppableId="Jobs">
+      {(provided) => (
+        <Card style={{ width: "100%" }}{...provided.droppableProps} ref={provided.innerRef}>
+<ListGroup variant="flush">
+          {jobList &&
+            jobList.map((job, index) => {
+              return (
+                <Draggable key={job} draggableId={job} index={index}>
+                  {(provided) => (
+                    <ListGroup.Item
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                   >
+                      <div>{job}</div>
+                      <Badge className="employeeBadge" bg="dark" pill>
+                0
               </Badge>
-            </ListGroup.Item>
-          ))}
-      </ListGroup>
-    </>
+                    </ListGroup.Item>
+                  )}
+                </Draggable>
+              );
+            })}
+          {provided.placeholder}
+          </ListGroup>
+        </Card>
+      )}
+    </Droppable>
+  </DragDropContext>
   );
 };
 
--- Client/src/pages/DesignJobs.js
+++ Client/src/pages/DesignJobs.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import ListGroup from "react-bootstrap/ListGroup";
 import logo from "../pictures/AFPlogo.png";
 import { useNavigate } from "react-router-dom";
 import { Col, Row } from "react-bootstrap";
@@ -8,15 +9,11 @@ import { Card } from "react-bootstrap";
 import "./DesignJobs.css";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import GetJobList from "../Components/DesignJobPageComponents/GetJobList";
-
+import "./Calendar.css";
 const DesignJobs = (props) => {
   const navigate = useNavigate();
-
-  const [tasks, setTasks] = useState([
-    { id: "1", content: "Task 1" },
-    { id: "2", content: "Task 2" },
-    { id: "3", content: "Task 3" },
-  ]);
+ 
+  const [tasks, setTasks] = useState(["test1", "test2","test3","teset4"]);
 
   const onDragEnd = (result) => {
     if (!result.destination) return;
@@ -30,7 +27,36 @@ const DesignJobs = (props) => {
 
   return (
     <>
-      {/* Your Navbar code */}
+      <Navbar
+        style={{
+          backgroundColor: "#90ee90",
+          paddingLeft: "16px",
+          paddingRight: "16px",
+          boxShadow: "0 4px 6px -6px #222",
+        }}
+        bg="dark"
+        data-bs-theme="dark"
+      >
+        <Nav className="me-auto"></Nav>
+        <Nav.Link
+          onClick={() => navigate("/DesignJobs")}
+          style={{ color: "white", fontStyle: "oblique",marginRight:"4%" }}
+        >
+          Design
+        </Nav.Link>
+        <Nav.Link
+          onClick={() => navigate("/Calendar")}
+          style={{ color: "white", fontStyle: "oblique" }}
+        >
+          Calendar
+        </Nav.Link>
+        <Navbar.Brand
+          style={{ cursor: "pointer", marginLeft: "20px" }}
+          onClick={() => navigate("/")}
+        >
+          <img className="logo" src={logo} />
+        </Navbar.Brand>
+      </Navbar>
       <Row style={{ height: "500px", width: "100%" }}>
         <Col className="col-2 employeeList">
           <div>
@@ -43,34 +69,30 @@ const DesignJobs = (props) => {
               <Card.Title>Done</Card.Title>
               <Card.Text>
                 <DragDropContext onDragEnd={onDragEnd}>
-                  <Droppable droppableId="done" type="CARD">
+                  <Droppable droppableId="Jobs">
                     {(provided) => (
-                      <div
-                        className="characters"
-                        ref={provided.innerRef}
-                        {...provided.droppableProps}
-                      >
-                        {tasks.map((item, index) => (
+                         <Card style={{ width: "100%" }}{...provided.droppableProps} ref={provided.innerRef}>
+                        {tasks.map((job, index) => (
                           <Draggable
-                            key={item.id}
-                            draggableId={item.id} // Ensure this matches the task ID
+                            key={job}
+                            draggableId={job} // Ensure this matches the task ID
                             index={index}
                           >
-                            
+                
                                                      {(provided) => (
-                              <span
+                              <ListGroup.Item
                                 ref={provided.innerRef}
                                 {...provided.draggableProps}
                                 {...provided.dragHandleProps}
                                 className="task-span" // Add a CSS class for styling
                               >
-                                {item.id}
-                              </span>
+                                {job}
+                              </ListGroup.Item>
                             )}
                           </Draggable>
                         ))}
                         {provided.placeholder}
-                      </div>
+                      </Card>
                     )}
                   </Droppable>
                 </DragDropContext>
