8e4e3c26feb83281631def9101521758d1e133a8
Paul Kearney <paulke@udel.edu>
2023-08-24 23:08:15 -0400

    fixed no access to PATCH

--- Server/server.js
+++ Server/server.js
@@ -9,7 +9,7 @@ const user_routes = require("./routes/user.js");
 const mongoose = require("mongoose");
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://acceleratedfp.onrender.com');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE','PATCH');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
